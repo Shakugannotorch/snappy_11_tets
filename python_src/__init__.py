585e49ea053a345319cf1158e30e8b4cae1dcaf8
Shana <903443276@qq.com>
2026-02-05 16:28:27 -0800

    Avoid circular import

--- python_src/__init__.py
+++ python_src/__init__.py
@@ -1,17 +1,7 @@
-__version__ = '1.17b'
+__version__ = '1.18b'
 
 def version():
     return __version__
 
 import sys
 from .database import get_tables, manifolds_path, original_manifolds_path
-
-try:
-    import snappy
-    table_dict = snappy.database.add_tables_from_package('snappy_11_tets', False)
-    for name, table in table_dict.items():
-        setattr(snappy, name, table)
-        if name not in snappy.database_objects:
-            snappy.database_objects.append(name)
-except ImportError:
-    pass
